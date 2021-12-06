47d602c329573fd85831b71151ecc24214130bf7
Mikolaj Panasiuk <m.panasiuk.14@outlook.com>
2021-12-06 20:38:20 +0100

    configure devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+    def check_logged_in
+        render json: {
+            "test": "test"
+        }
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get '/check_logged_in', to: 'sessions#check_logged_in', as: 'check_logged_in'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
