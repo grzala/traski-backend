9a63843d353f351beb40438fd37605186a39e6a6
Mikolaj Panasiuk <m.panasiuk.14@outlook.com>
2021-12-07 12:26:02 +0100

    working on session check logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,44 @@
 class SessionsController < ApplicationController
 
     def create
+        error = false
+        erorr_msg = ""
+
+        user = User.find_by_email(params[:user][:email])
+        if (!user)
+            error = true
+            error_msg = "There is no user with email: " + params[:user][:email]
+        end
+        
+        if (!error)
+            if(!user.valid_password?(params[:user][:password]))
+                error = true
+                error_msg = "Email and password do not match"
+            end
+        end
+
+        if (error)
+            render :json => {
+                :error_msgs => [error_msg]
+            }, :status => 401
+        else
+            render :json => {
+                :notice => "Successfully logged in as: " + params[:user][:password]
+            }
+        end
+        
+        # login_as(@user1, :scope => :user)
+
+        # render :json => {
+        #     :asdasdsa => "asdasd"
+        # }, :status => 500
+    end
+
+    def check_logged_in
+
 
         render :json => {
-            :asdasdsa => "asdasd"
+            "yoyoyoyo": "yoyoyoyo"
         }
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :sessions, only: [:create]
+  resources :sessions, only: [:create] 
+
+  controller :sessions do
+    get '/check_logged_in', action: :check_logged_in, as: :checked_logged_in
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
