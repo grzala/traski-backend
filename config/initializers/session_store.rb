2afa2676dcfcbe6a1fe9b260039a55e1c7851439
Mikolaj Panasiuk <m.panasiuk.14@outlook.com>
2021-12-07 11:16:36 +0100

    working on cors

--- config/initializers/session_store.rb
+++
@@ -1,7 +0,0 @@
-
-if Rails.env == "production"
-    # CHANGE IN PROD
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "mydomain"
-else 
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app"
-end
