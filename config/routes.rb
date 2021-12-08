93e9cb4f85268903e1a23a9de7416cdf4acd410d
Mikolaj Panasiuk <m.panasiuk.14@outlook.com>
2021-12-08 15:05:53 +0100

    add moto_routes to rotues

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
     delete '/session', action: :destroy, as: :logout
   end
 
+  resources :moto_routes
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
