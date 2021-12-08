bd11e918f02a5444b2b3b729ff863c83d17625f1
Mikolaj Panasiuk <m.panasiuk.14@outlook.com>
2021-12-08 11:43:41 +0100

    install rspec and factory bot

---
+++ db/migrate/20211208103641_create_moto_routes.rb
@@ -0,0 +1,17 @@
+class CreateMotoRoutes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :moto_routes do |t|
+      t.string :name
+      t.string :description
+      t.string :coordinates_json_string
+      t.date :open_start
+      t.date :open_end
+      t.integer :time_to_domplete_h
+      t.integer :time_to_complete_m
+      t.integer :difficulty
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
