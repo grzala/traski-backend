e80a59be952ecd3186adbef7d5b8e4456606e599
Mikolaj Panasiuk <m.panasiuk.14@outlook.com>
2021-12-14 13:23:35 +0100

    use cookiesstore

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,9 @@ module TraskiBackend
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.insert_after(ActionDispatch::Cookies, ActionDispatch::Session::CookieStore)
+
   end
 end
