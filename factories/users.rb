0120db057b9c8ba244a417aa64fc87d9d973492a
Mikolaj Panasiuk <m.panasiuk.14@outlook.com>
2022-01-20 19:08:28 +0100

    add a check to prevent creating factory before migrating

--- factories/users.rb
+++ factories/users.rb
@@ -26,19 +26,20 @@ last_names = [
 ]
 
 
+if ActiveReco7rd::Base.connection.data_source_exists? 'users'
+    FactoryBot.define do
+        factory :user do
+            sequence(:email, User.last.nil? ? 1 : User.last.id + 1) { |n| "test#{n}@mail.com" }
+            password { "testtest" }
+            password_confirmation { "testtest" }
+            first_name { first_names.sample }
+            last_name { last_names.sample }
 
-FactoryBot.define do
-    factory :user do
-        sequence(:email, User.last.nil? ? 1 : User.last.id + 1) { |n| "test#{n}@mail.com" }
-        password { "testtest" }
-        password_confirmation { "testtest" }
-        first_name { first_names.sample }
-        last_name { last_names.sample }
 
+            # after :create do |user|
+            #     create_list :post, 3, user: user
+            # end
+        end
 
-        # after :create do |user|
-        #     create_list :post, 3, user: user
-        # end
     end
-
 end
