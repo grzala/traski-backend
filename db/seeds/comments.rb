dd327a642f6bfe5d0b8a115c6afe8320dee9998f
Mikolaj Panasiuk <m.panasiuk.14@outlook.com>
2022-01-20 14:33:54 +0100

    add sample comments

--- db/seeds/comments.rb
+++ db/seeds/comments.rb
@@ -3,14 +3,32 @@ module CommentsSeed
 
     COMMENTS = {
         POSITIVE: [
-            "  pozytywnie"
+            "I loved this route",
+            "Beautiful scenery and not a lot of traffic",
+            "Nice turns",
+            "I spent a fantastic weekend there a year ago",
+            "I hope I go back there soon",
+            "All around fantastic route",
+            "What ride",
+            "Great",
+            "Love it. Thanks for posting",
         ],
         NEGATIVE: [
-            "  negativ"
+            "Lots of traffic",
+            "Boring",
+            "I hope I never return there",
+            "Restaurants are closed and urbex spots are no longer accessible",
+            "Dangerous route. Avoid if possible",
+            "Horrible"
         ],
         INDIFFERENT: [
-
-            "  tak se"
+            "I've had fun but I won't be riding it again",
+            "Boring but good to develop skills as a beginner",
+            "Not much to see there",
+            "I've found the speed limits a bit harsh",
+            "Meh",
+            "There are better routes in the area",
+            "Decent"
         ]
     }
     
