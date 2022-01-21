6d906743b3e7872aa70fcd052beb2e8e3ba03e10
Mikolaj Panasiuk <m.panasiuk.14@outlook.com>
2022-01-21 15:00:11 +0100

    change 'date_created' to 'created_at'

--- app/models/moto_route_favourite.rb
+++ app/models/moto_route_favourite.rb
@@ -3,7 +3,7 @@ class MotoRouteFavourite < ApplicationRecord
     belongs_to :moto_route
 
     PER_PAGE_USER_FAVOURITES = 10
-    scope :get_user_favourites, -> (user, offset) { where(user: user).order(date_created: :desc).limit(PER_PAGE_USER_FAVOURITES).offset(offset) }
+    scope :get_user_favourites, -> (user, offset) { where(user: user).order(created_at: :desc).limit(PER_PAGE_USER_FAVOURITES).offset(offset) }
 
 
 end
